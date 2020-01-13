c15ccf7d1ff47b5efb03ee1d5e2bd908c827a882
Alexandr Romanov <romanov.spb.alex@yandex.ru>
2020-01-13 03:25:23 +0300

    Procfile, hello darling

--- main.go
+++ main.go
@@ -1,36 +1,36 @@
 package main
 
 import (
-  "os"
-  "log"
-  tb "gopkg.in/tucnak/telebot.v2"
+	"os"
+	"log"
+	tb "gopkg.in/tucnak/telebot.v2"
 )
 
 func main() {
-  var (
-    port      = os.Getenv("PORT")       // sets automatically
-    publicURL = os.Getenv("PUBLIC_URL") // you must add it to your config vars
-    token     = os.Getenv("TOKEN")      // you must add it to your config vars
-  )
+	var (
+		port      = os.Getenv("PORT")       // sets automatically
+		publicURL = os.Getenv("PUBLIC_URL") // you must add it to your config vars
+		token     = os.Getenv("TOKEN")      // you must add it to your config vars
+	)
 
-  webhook := &tb.Webhook{
-    Listen:   ":" + port,
-    Endpoint: &tb.WebhookEndpoint{PublicURL: publicURL},
-  }
+	webhook := &tb.Webhook{
+		Listen:   ":" + port,
+		Endpoint: &tb.WebhookEndpoint{PublicURL: publicURL},
+	}
 
-  pref := tb.Settings{
-    Token:  token,
-    Poller: webhook,
-  }
+	pref := tb.Settings{
+		Token:  token,
+		Poller: webhook,
+	}
 
-  b, err := tb.NewBot(pref)
-  if err != nil {
-    log.Fatal(err)
-  }
+	b, err := tb.NewBot(pref)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-  b.Handle("/hello", func(m *tb.Message) {
-    b.Send(m.Sender, "Hi!")
-  })
+	b.Handle("/hello", func(m *tb.Message) {
+		b.Send(m.Sender, "Hi!")
+	})
 
-  b.Start()
+	b.Start()
 }
